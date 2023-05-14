afd13f8254c551999f9e2ee23aa580b42fbcb66c
dusanr14 <dusanr14@gmail.com>
2023-05-14 20:20:07 +0200

    Initial commit

---
+++ drugi_pokusaj/eeIndex/src/eeindex/Student.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eeindex;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dusan
+ */
+public class Student {
+    private String name;
+    private String lastName;
+    private String jmbg;
+    private String userName;
+    private String password;
+    private int numOfSubjects;
+    private  ArrayList<Subject> subjects;
+
+    public Student(String name, String lastName, String jmbg, String userName, String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.jmbg = jmbg;
+        this.userName = userName;
+        this.password = password;
+        this.numOfSubjects = 0;
+    }
+
+    public Student(String name, String lastName, String jmbg, String userName, String password, int numOfSubjects, ArrayList<Subject> subjects) {
+        this.name = name;
+        this.lastName = lastName;
+        this.jmbg = jmbg;
+        this.userName = userName;
+        this.password = password;
+        this.numOfSubjects = numOfSubjects;
+        this.subjects = subjects;
+    }
+    
+        
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getJmbg() {
+        return jmbg;
+    }
+
+    public void setJmbg(String jmbg) {
+        this.jmbg = jmbg;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(ArrayList<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public int getNumOfSubjects() {
+        return numOfSubjects;
+    }
+
+    public void setNumOfSubjects(int numOfSubjects) {
+        this.numOfSubjects = numOfSubjects;
+    }
+    
+    public void printSubject(){
+        String string;
+        string = this.getName()+" "+this.getLastName()+" "+this.getJmbg()+" "+this.getUserName()+" "+this.getPassword()+" "+this.getNumOfSubjects()+" ";
+        for(Subject s: subjects){
+            string += s.printSubject();
+        }
+        System.out.println(string);
+    }
+    
+}
---
+++ drugi_pokusaj/eeIndex/src/eeindex/Subject.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eeindex;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dusan
+ */
+class Subject {
+    private  String subjectName;
+    private int numberOfParts;
+    private ArrayList<String> partsName;
+    private  ArrayList<Integer> maxPoints;
+    private ArrayList<Integer> minPoints;
+    private ArrayList<Integer> obtainedPoints;
+    private int grade;
+            
+    public Subject(String subjectName, int numberOfParts,ArrayList<String> partsName, ArrayList<Integer> maxPoints, ArrayList<Integer> minPoints) {
+        this.subjectName = subjectName;
+        this.numberOfParts = numberOfParts;
+        this.partsName = partsName;
+        this.maxPoints = maxPoints;
+        this.minPoints = minPoints;
+    }
+
+    public Subject(String subjectName, int numberOfParts, ArrayList<String> partsName, ArrayList<Integer> maxPoints, ArrayList<Integer> minPoints, ArrayList<Integer> obtainedPoints, int grade) {
+        this.subjectName = subjectName;
+        this.numberOfParts = numberOfParts;
+        this.partsName = partsName;
+        this.maxPoints = maxPoints;
+        this.minPoints = minPoints;
+        this.obtainedPoints = obtainedPoints;
+        this.grade = grade;
+    }
+
+    public ArrayList<String> getParts_name() {
+        return partsName;
+    }
+
+    public int getNumberOfParts() {
+        return numberOfParts;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public ArrayList<Integer> getMaxPoints() {
+        return maxPoints;
+    }
+
+    public ArrayList<Integer> getMinPoints() {
+        return minPoints;
+    }
+
+    public ArrayList<Integer> getObtainedPoints() {
+        return obtainedPoints;
+    }
+
+    public void setObtained_points(ArrayList<Integer> obtained_points) {
+        this.obtainedPoints = obtained_points;
+    }
+
+    public int getGrade() {
+        /*int temp;
+        boolean fail = false;
+        for(int i = 0;i < this.getNumberOfParts(); i++){
+            if(obtainedPoints.get(i) < minPoints.get(i)){
+                fail = true;
+            }
+        }
+        */
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+    
+    public String printSubject(){
+        String string;
+        string = this.getSubjectName()+" "+this.getNumberOfParts()+" "+this.getMaxPoints()
+                +" "+this.getMinPoints()+" "+this.getObtainedPoints()+" "+this.getGrade();
+        return string;
+    }
+    
+}
